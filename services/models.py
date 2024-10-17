5cff19dfb9265c28a7de03f34f7557007b6fa329
Lusayo Nyondo <lusayohendy@gmail.com>
2024-10-17 22:39:58 +0200

    Fixed spacing for project technology filters.

--- services/models.py
+++ services/models.py
@@ -1,3 +1,19 @@
+import os
 from django.db import models
 
-# Create your models here.
+from portfolio.validators import (
+    validate_icon_file_extension
+)
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.FileField(
+        upload_to=os.path.join('services', 'thumbnails'),
+        validators=[validate_icon_file_extension],
+    )
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
